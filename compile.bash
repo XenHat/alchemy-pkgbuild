64d7f120ca2f31f5b5e144e8ec7e04654c2dcb19
XenHat <me@xenh.at>
2024-01-08 17:57:37 -0500

    Add regular build script that pulls from the templates

---
+++ compile.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+echo "The script you are running has:"
+echo "basename: [$(basename "$0")]"
+echo "dirname : [$(dirname "$0")]"
+echo "pwd     : [$(pwd)]"
+
+srcdir=$PWD
+dir_name=$(dirname "$0")
+
+source "${dir_name}"/prepare-1.0.template
+source "${dir_name}"/build-1.0.template
+
+cleanbuild() {
+	rm -rf build-linux-64
+	git pull --autostash
+	prepare
+	build
+}
+prepare
+build
+
+# vi: set ai softtabstop=2 shiftwidth=2 tabstop=2 expandtab:
